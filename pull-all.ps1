61910f6ab1dd037b629596e9fa20958d9ffbb13a
beta382 <beta382@gmail.com>
2014-03-23 22:37:00 -0500

    More nice things

--- pull-all.ps1
+++ pull-all.ps1
@@ -1,7 +1,8 @@
+echo ""
 Get-ChildItem | ? { $_.PSIsContainer } | % { `
     Push-Location $_.FullName;               `
-    echo "Pulling to $_";                    `
+    echo "Pulling to $_...";                 `
     git pull origin master 2> $null;         `
-    echo "";                                    `
+    echo "";                                 `
     Pop-Location                             `
 }
